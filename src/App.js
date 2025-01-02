0e7ac2910f8a2e9c169c006478f5d2f17639cc48
Maviz Ali <mavizali@syscrowd.com>
2025-01-02 11:55:56 -0800

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Maviz and Viki React
         </a>
       </header>
     </div>
